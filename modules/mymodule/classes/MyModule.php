4bac161792eb5399644bdffca5988bc2d251e27d
quentin <zhor.osu@gmail.com>
2017-07-04 16:34:36 +0200

    initial commit

---
+++ modules/mymodule/classes/MyModule.php
@@ -0,0 +1,26 @@
+<?php
+
+class MyModule extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'mymodule';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Jean-Jeannine De La Tourette';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Doge Module lel 8D');
+        $this->description = $this->l('Gné wassup yo?');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+}
---
+++ modules/mymodule/mymodule.php
@@ -0,0 +1,5 @@
+<?php
+require_once _PS_MODULE_DIR_.'/mymodule/classes/MyModule.php';
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
